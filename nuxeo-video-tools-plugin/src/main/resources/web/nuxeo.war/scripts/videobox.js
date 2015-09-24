bdc3e25f7af00a19b2e27e95f73b873aa304d0cb
Michaël Vachette <mvachette@nuxeo.com>
2015-09-24 15:11:14 -0700

    Updated videobox

--- nuxeo-video-tools-plugin/src/main/resources/web/nuxeo.war/scripts/videobox.js
+++ nuxeo-video-tools-plugin/src/main/resources/web/nuxeo.war/scripts/videobox.js
@@ -1,15 +1,35 @@
-function initVideoJs(videoElement) {
+function initVideoJs(videoElement,sliderOn) {
 
   var video = vjs(videoElement);
-  video.rangeslider({hidden:false});
   video.hideControlTime();
 
-  var start = jQuery('#videobox_start_timestamp :input').first();
-  var end = jQuery('#videobox_end_timestamp :input').first();
+  // set sliders
+  if (sliderOn === 'true') {
+    video.rangeslider({hidden:false});
+  } else {
+    video.rangeslider({hidden:true});
+  }
 
-  video.on("sliderchange",function() {
-    var values = video.getValueSlider();
-    start.attr('value',''+videojs.round(values.start,3));
-    end.attr('value',videojs.round(values.end,3));
+  var current = jQuery('#videobox_current_timestamp :input').first();
+
+  video.on('timeupdate',function() {
+    if (current !== undefined) current.attr('value',''+videojs.round(video.currentTime(),3));
   });
+
+  if (sliderOn === 'true') {
+    var start = jQuery('#videobox_start_timestamp :input').first();
+    var end = jQuery('#videobox_end_timestamp :input').first();
+
+    video.on('sliderchange',function() {
+      var values = video.getValueSlider();
+      if (start !== undefined) start.attr('value',''+videojs.round(values.start,3));
+      if (end !== undefined) end.attr('value',videojs.round(values.end,3));
+    });
+  }
+
+  video.ready(function(){
+    this.play();
+    this.pause();
+  });
+
 }
